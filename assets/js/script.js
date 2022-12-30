f0ef890455d08144ca96b8de47b7101c86e8948e
matteo <matteo.razzanelli89@gmail.com>
2022-12-30 15:43:31 +0100

    first qorking version

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,29 @@ const minus = document.getElementById('minus');
 const softReset = document.getElementById('soft-reset');
 const hardReset = document.getElementById('hard-reset');
 let currentValue = document.getElementById('current-value');
+let minusNotifications = document.getElementById('minus-notifications');
+let plusNotifications = document.getElementById('plus-notifications');
 
 plus.addEventListener("click", ()=>{
   currentValue.innerHTML++;
-  print();
+  plusNotifications.innerHTML++;
 });
 
 minus.addEventListener("click", ()=>{
   currentValue.innerHTML--;
-  print();
+  minusNotifications.innerHTML++;
 });
 
-function print(){
-  console.log(currentValue.innerHTML);
-}
+softReset.addEventListener("click", ()=>{
+  if(confirm("Reset counter?")){
+    currentValue.innerHTML=0;
+  }
+})
+
+hardReset.addEventListener("click", ()=>{
+  if(confirm("Do you want to reset counter and notifications?")){
+    currentValue.innerHTML=0;
+    plusNotifications.innerHTML=0;
+    minusNotifications.innerHTML=0;
+  }
+})
